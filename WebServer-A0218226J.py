4a83375582aee51c2707c4cf8eb51d9650a80fe5
Ho Lanzan (Leon) <ho.leon98@gmail.com>
2022-09-04 17:18:13 +0800

    Fix issues with code

--- WebServer-A0218226J.py
+++ WebServer-A0218226J.py
@@ -6,7 +6,6 @@ serverPort = int(sys.argv[1])
 serverSocket = socket(AF_INET,SOCK_STREAM) 
 serverSocket.bind(('',serverPort))
 serverSocket.listen(1) 
-connectionSocket, clientAddr = serverSocket.accept()
 
 # key-value store
 key_value = {}
@@ -21,7 +20,7 @@ def parse(header):
     method = split[0]
     path = (split[1].split("/"))[1]
     key = (split[1].split("/"))[2]
-    len = -1
+    length = -1
 
     if (not (path == "key" or path == "counter")): # path here is case-sensitive
         path = ""
@@ -29,11 +28,15 @@ def parse(header):
     if (method.upper() == "GET" or method.upper() == "DELETE"):
         return [method, path, key]
 
-    if ("Content-Length" in split):
-        idx = split.index("Content-Length")
-        len = int(split[idx + 1])
-    
-    return [method, path, key, len]
+    if ("content-length" in header.lower()):
+        startIdx = header.lower().find("content-length") + 15
+        print (startIdx)
+        other = header[startIdx:]
+        endIdx = other.find(" ") + startIdx
+        print(endIdx)
+        length = int(header[startIdx:endIdx])
+
+    return [method, path, key, length]
 
 # Read header request 1 byte at a time to account for intermittent transmission
 def execute(parsedMessage):
@@ -43,7 +46,7 @@ def execute(parsedMessage):
     
     if method == "GET":
         if key not in key_value:
-            return '404 NotFound  '
+            return '404 NotFound  '.encode()
         # get key-value
         if path == "key":
             body = key_value.get(key)
@@ -54,75 +57,81 @@ def execute(parsedMessage):
                 if counter[key] <= 0:
                     counter.pop(key, None)
                     key_value.pop(key, None)
+            return '200 OK Content-Length '.encode() + str(len(body)).encode() + b'  ' + body
 
         # get counter
         if path == "counter":
             body = str(counter.get(key, 'Infinity'))
-        
-        return '200 OK Content-Length '.encode() + str(len(body)).encode() + '  ' + body.encode()
+            return '200 OK Content-Length '.encode() + str(len(body)).encode() + b'  ' + body.encode()
 
     if method == "POST":
         length = parsedMessage[3]
+
         # POST key-value
         if path == "key":
-            if key in counter:
-                return '405 MethodNotAllowed  '
             value = b'' # value is a binary string
-            while (len(value) < max(length, FIVE_MB)):
+            for i in range (min(length, FIVE_MB)):
                 value += connectionSocket.recv(1) # handle intermittent connection
+            if key in counter:
+                return '405 MethodNotAllowed  '.encode()
             key_value.update({key: value})
+            return '200 OK  '.encode()
+
         # POST counter
         if path == "counter":
             # key does not exist in key-value
             if key not in key_value:
-                return '405 MethodNotAllowed  '
+                return '405 MethodNotAllowed  '.encode()
             # key does not exist in key-value
             else:
                 count = b''
-                while (len(count) < max(length, FIVE_MB)):
+                for i in range (min(length, FIVE_MB)):
                     count += connectionSocket.recv(1) # handle intermittent connection
-                count = int(value)
-                # insertion
+                # count is an int
+                count = int(count)
+                # insertion 
                 if counter.get(key) == None:
                     counter.update({key: count})
                 # update
                 else:
                     counter[key] += count
-                return '200 OK  '
+                return '200 OK  '.encode()
 
     if method == "DELETE":
         # non-existent key
         if key not in key_value:
-            return '404 NotFound  '
+            return '404 NotFound  '.encode()
         # DELETE key-value
         if path == "key":
             # counter value does not exist
-            if key not in count:
-                body = str(key_value.pop(key, None))
-                return '200 OK Content-Length '.encode() + str(len(body)).encode() + '  ' + body.encode()
+            if key not in counter:
+                body = key_value.pop(key, None)
+                return '200 OK Content-Length '.encode() + str(len(body)).encode() + b'  ' + body
             else: 
-                return '405 MethodNotAllowed  '
+                return '405 MethodNotAllowed  '.encode()
         # DELETE counter
         if path == "counter":
             # counter value does not exist
-            if key not in count:
-                return '404 NotFound  '
+            if key not in counter:
+                return '404 NotFound  '.encode()
             # counter value exist
             else:
                 body = str(counter.pop(key, None))
-                return '200 OK Content-Length ' + str(len(body)).encode() + '  ' + body.encode()
+                return '200 OK Content-Length '.encode() + str(len(body)).encode() + b'  ' + body.encode()
 
 while True:
-    message = connectionSocket.recv(1)
+    connectionSocket, clientAddr = serverSocket.accept()
+    while True:
+        message = connectionSocket.recv(1)
 
-    if len(message) == 0:
-        break
+        if len(message) == 0:
+            break
 
-    while (message.find(b'  ') == -1):
-        message += connectionSocket.recv(1)
-    
-    output = execute(parse(message.decode()))
-    connectionSocket.send(output)
+        while (message.find(b'  ') == -1):
+            message += connectionSocket.recv(1)
+
+        output = execute(parse(message.decode()))
+        connectionSocket.send(output)
 
 connectionSocket.close()
 
